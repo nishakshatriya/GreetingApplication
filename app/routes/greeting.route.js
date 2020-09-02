9ed4c4f081f3d94f352864150ddda99919636131
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-02 20:46:03 +0530

    Add: Find greeting by greeting id

--- app/routes/greeting.route.js
+++ app/routes/greeting.route.js
@@ -3,4 +3,5 @@ module.exports = (app) => {
 
     app.post('/greetings', greetings.create);
     app.get('/greetings', greetings.findAll);
+    app.get('/greetings/:greetingId',greetings.findOne);
 }
